fcc754970473c967e2a2cf8197c55c489946a232
Moustafa <moustafasaper167@gmail.com>
2024-05-12 01:40:18 +0300

    changed some dark/light mode configurations

--- lib/features/cart/presentation/widgets/custom_app_bar.dart
+++ lib/features/cart/presentation/widgets/custom_app_bar.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:medcs/core/constent/colors.dart';
 import 'package:medcs/core/utlity/styles.dart';
+import 'package:medcs/features/home/prsentation/manger/them_provider/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class CustomAppBar extends StatelessWidget {
   final String title;
@@ -13,10 +16,13 @@ class CustomAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
     return Container(
       height: kToolbarHeight, // Use the same height as AppBar
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      color: Colors.white, // Customize the background color
+      color: themeProvider.isDarkMode
+          ? AppColors.scaffoldDarkMode
+          : Colors.white, // Customize the background color
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -24,13 +30,15 @@ class CustomAppBar extends StatelessWidget {
             padding: const EdgeInsets.only(left: 120),
             child: Text(
               title,
-              style: StylesLight.bodyLarge17,
+              style: themeProvider.isDarkMode
+                  ? StylesDark.bodyLarge17
+                  : StylesLight.bodyLarge17,
             ),
           ),
           IconButton(
             onPressed: onDeletePressed,
             icon: const Icon(Icons.delete),
-            color: Colors.black,
+            color: Colors.red,
           ),
         ],
       ),
